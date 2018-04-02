627549477a5349ebc1e816fcef08cc9b5d70e5ab
Sam <sam@developersam.com>
2018-04-02 17:41:39 -0400

    Added tests

---
+++ src/test/kotlin/com/developersam/game/ten/TenPerformanceTest.kt
@@ -0,0 +1,19 @@
+package com.developersam.game.ten
+
+import org.junit.Test
+
+/**
+ * Run a performance test on TEN.
+ */
+class TenPerformanceTest {
+
+    /**
+     * Test the performance of the game by run a game between two MCTS AI.
+     */
+    @Test
+    fun testPerformance() {
+        TenBoard.runAGameBetweenTwoAIs(
+                aiThinkingTime = 50, printGameStatus = false)
+    }
+
+}
